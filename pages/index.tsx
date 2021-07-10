be9231dc4b2c8b465d977cf4839d493829309441
Henrique <ch.henriquevinha@gmail.com>
2021-07-10 02:27:44 -0300

    Config tsx and organization index

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div className={styles.container}>
+          <h1 className={styles.title}> Welcome to <a href="https://nextjs.org"> Next.js!</a></h1>
+        </div>
+      </main>
+    </>
+
+  )
+}
