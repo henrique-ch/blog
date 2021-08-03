044948c58dd2c407b25dc2fc391d14ce6ce6804c
henrique <ch.henriquevinha@gmail.com>
2021-08-03 15:06:46 -0300

    Add image in home

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,9 +1,14 @@
 import styles from './header.module.css'
+import Image from 'next/image'
+import profilePic from 'public/programmer.png'
+
 
 export default function header(){
     return (
         <header className={styles.container}>
-          <h1 className={styles.title}> Welcome to <a href="https://nextjs.org"> Next.js!</a></h1>
+          <h1 className={styles.title}> Carlos Henrique</h1>
+	        <h3 className={styles.title}> Backend Developer</h3>
+          <Image src={profilePic} alt="Picture of the author" />
         </header>
     )
 }
